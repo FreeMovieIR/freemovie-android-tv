1a6e085ce602033cabfe5e8576560e4ff70ebc02
Rick Sanchez <matinbeigi99@gmail.com>
2025-04-01 00:09:14 +0330

    auto build

--- lib/utils/web/http_client.dart
+++ lib/utils/web/http_client.dart
@@ -1,9 +1,19 @@
 import 'package:dio/dio.dart';
 
 final Dio tmdbHttpClient = Dio(
-  BaseOptions(baseUrl: const String.fromEnvironment('TMDB_BASE_URL')),
+  BaseOptions(
+    baseUrl: const String.fromEnvironment('TMDB_BASE_URL', defaultValue: 'https://api.themoviedb.org/3/'),
+    headers: {
+      'Authorization': 'Bearer ${const String.fromEnvironment('TMDB_API_KEY')}',
+    },
+  ),
 );
 
 final Dio omdbHttpClient = Dio(
-  BaseOptions(baseUrl: const String.fromEnvironment('OMDB_BASE_URL')),
+  BaseOptions(
+    baseUrl: const String.fromEnvironment('OMDB_BASE_URL', defaultValue: 'http://www.omdbapi.com/'),
+    queryParameters: {
+      'apikey': const String.fromEnvironment('OMDB_API_KEY'),
+    },
+  ),
 );
