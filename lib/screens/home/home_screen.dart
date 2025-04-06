6cfa42d2ef2fe52ab37b3758f2670cda7a4b7c99
AmirArani <blackstar.m77@gmail.com>
2025-04-06 19:16:09 +0330

    1404/01/17 | update-ui | fix announcement banner layout bug

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -231,40 +231,7 @@ class _HomeViewState extends State<_HomeView> {
               padding: const EdgeInsets.symmetric(vertical: 16),
               children: [
                 /// Announcement banner
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  margin: EdgeInsets.symmetric(vertical: 16, horizontal: 72),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  child: Row(
-                    children: [
-                      Assets.images.icons.infoFill.svg(),
-                      SizedBox(width: 12),
-                      Text('بیانیه سلب مسئولیت', style: TextStyle(fontSize: 12)),
-                      Container(
-                          height: 3,
-                          width: 3,
-                          margin: EdgeInsets.all(8),
-                          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white60)),
-                      Flexible(
-                        child: Text(
-                            ' این وب‌سایت فقط اطلاعات فیلم و سریال را نمایش می‌دهد و هیچ محتوایی را میزبانی نمی‌کند. اطلاعات از APIهای عمومی و لینک‌های دانلود از الماس مووی دریافت می‌شود',
-                            style: TextStyle(fontSize: 11)),
-                      ),
-                      TextButton(
-                          onPressed: () {},
-                          child: Row(
-                            spacing: 8,
-                            children: [
-                              Text('اطلاعات بیشتر', style: TextStyle(fontSize: 11)),
-                              Icon(Icons.chevron_right)
-                            ],
-                          ))
-                    ],
-                  ),
-                ),
+                AnnouncementBanner(),
 
                 /// Latest Movies Section
                 if (latestMovies.isNotEmpty)
@@ -363,6 +330,65 @@ class _HomeViewState extends State<_HomeView> {
           borderRadius: BorderRadius.circular(12),
         ),
         child: child,
+      ),
+    );
+  }
+}
+
+class AnnouncementBanner extends StatelessWidget {
+  const AnnouncementBanner({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: EdgeInsets.symmetric(vertical: 16, horizontal: 72),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Icon
+          Assets.images.icons.infoFill.svg(),
+          SizedBox(width: 12),
+          Text('بیانیه سلب مسئولیت', style: TextStyle(fontSize: 12)),
+          Container(
+            height: 3,
+            width: 3,
+            margin: EdgeInsets.all(8),
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white60),
+          ),
+          // Title and text
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  ' این وب‌سایت فقط اطلاعات فیلم و سریال را نمایش می‌دهد و هیچ محتوایی را میزبانی نمی‌کند. اطلاعات از APIهای عمومی و لینک‌های دانلود از الماس مووی دریافت می‌شود',
+                  style: TextStyle(fontSize: 11),
+                  softWrap: true,
+                ),
+              ],
+            ),
+          ),
+
+          // Button
+          TextButton(
+            onPressed: () {},
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('اطلاعات بیشتر', style: TextStyle(fontSize: 11)),
+                Icon(Icons.chevron_right)
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
